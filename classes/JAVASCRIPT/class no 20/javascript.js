2deaaf4cda54b68ac9919799e2f9c167fdbb1f28
maira zahid <mahmoodzahid105@gmail.com>
2026-03-06 11:21:42 -0800

    Added extra lines to JavaScript file

---
+++ classes/JAVASCRIPT/class no 20/javascript.js
@@ -0,0 +1,127 @@
+// console.log('Hello')
+// alert('Hello Alert')
+
+// x = 10
+// variable -> value 
+// Register Form 
+
+/*username= 'Fiza'
+email='fiza@gmail.com'
+city='Faisalabad'
+code= 38000*/
+
+// console.log(second_no0)
+// single quote 
+// console.log('Hello')
+// double quote 
+// console.log("Hello's")
+
+// no quotation for calling a function 
+// console.log(username)
+// console.log(email)
+// console.log(city)
+// console.log(code)
+
+// saperate by comma 
+// console.log(username , email , city , code)
+
+
+
+// numbers ko string me nahi likhna 
+
+// code
+// characters
+// string '', ""
+
+// string , number -> datatype 
+// variable 
+
+
+// task no 10 :-                      extra = ctrl+ F for find 
+// var , const , let 
+// data types (string , number , boolean , null , undefined ,null , typeof) 
+// prompt 
+
+// text => string 
+// variable -> assign  -> value 
+// username=''
+// $ dollar ka sign variable name se pehle use krna acha h
+
+// data -> table 
+//  excel file: CSV data in comma
+// 123 , fatima , 505
+// 341 , noor , 707
+
+// $user$123name=''
+// _username=''
+// $user123name='data';
+
+// var           can do anything
+// var username ='arshad' 
+// console.log(username)
+// const          nothing can do
+// const username ='fatima'
+// username ='arshad'
+// console.log(username)
+// let            only value can declared       for makig big websites , mostly let is used
+// let rollno; 
+// rollno = 1234
+
+// prompt
+// let email;
+// console.log(email)
+// email = prompt("Enter your Email:")
+// console.log(email) 
+// document.getElementById("demo").innerHTML=`<h1> ${email}</h1> `;
+// use litteral by left btn of 1 ( ` text of prompt`)
+
+// text + var
+//const username = 'fatima'
+// console.log('hello' , username , 123 , 'welcome' )
+// to automatically write the console.log we have to write (clg) after installing extension ( html , css , javascript snippe)
+
+//const username = 'fatima'
+// console.log( 'hello username')
+
+/*const username = 'fatima'
+console.log(`hello  ${username}`)*/
+
+// var in string 
+// double click kr k select ho gaiy ga then copy paste kr sakte h (technique for writting var name)
+
+
+// make a profile of linkedin
+
+
+// operator
+// arithmetic(* , + , - , %)
+// assighnment (=)
+// comparison (> , < , <= , >=)
+
+// ctrl+F for changing the numbers with var name 
+
+// operands:
+
+// method no 01
+/*let first_no = 3
+let second_no = 4
+console.log(first_no+second_no) //6
+console.log(first_no-second_no) //-2
+console.log(first_no*second_no)  //8
+console.log(first_no%second_no)  //0
+console.log(second_no%first_no)  //2
+console.log(first_no/second_no)*/ //0.5 
+
+// method no 02
+/*let first = 3
+let second = 4
+let result ;
+result = first + second
+result = first - second
+result = first * second
+result = first % second
+result = first / second
+console.log(result) */
+
+
+//linkedin signup
---
+++ practice/prac-of-js/script.js
@@ -0,0 +1,30 @@
+ //show the value stored in the variable
+ var username ='arshad' 
+ console.log(username) 
+ // show the type of value stored in the const if we have define it for more time
+const uname ='fatima'
+var user ='arshad'
+console.log(uname) 
+console.log(user)
+//use of let 
+let rollno
+rollno = 1234 //not displayed
+//2nd try
+let x = 45
+console.log(x)
+//Promt
+let number 
+console.log(number);
+number = prompt("Enter your phone number")
+console.log(number);
+document.getElementById("head").innerHTML= `<h1> ${number} </h1>`;
+//2nd
+let fullname
+console.log(fullname)
+fullname =  prompt("Enter your full name")
+console.log(fullname);
+document.getElementById("demo").innerHTML=`<h1> ${fullname}</h1>`
+//text with variables
+const word = 'fatima'
+console.log('hello' , word , 123 , 'welcome' ) 
+
