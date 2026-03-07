8ee18c387bb00fbf8b827a1486315704acff0b3e
maira zahid <mahmoodzahid105@gmail.com>
2026-03-07 01:38:06 -0800

    javascript.js

--- classes/JAVASCRIPT/class no 21/javascript.js
+++ classes/JAVASCRIPT/class no 21/javascript.js
@@ -1,25 +1,31 @@
 // use Claude.ai
 
-//increment operator
-let first_no = 2
-first_no++
-console.log(first_no);//3
-first_no = first_no + 1
-console.log(first_no);//4
+// let first_no = 2
+// first_no++
+// console.log(first_no) // 3
+// first_no = first_no + 1 
+// console.log(first_no) // 4
+// first_no--
+// console.log(first_no) // 3
+// console.log(First no  is ${first_no}+1)
+// console.log(First no  is , first_no + 1 )
+// let text1 = "John";
+// let text2 = "Doe";
+// let text3 = text1 + "" + text2
+// console.log(text3)
 
-//Comparison operator
-let value1 = 10
-let value2 = 20
-console.log(value1 > value2);//false
-console.log(value1 < value2); 
-console.log(value1 > value2); 
-console.log(value1 <= value2);
-console.log(value1 == value2);
-console.log(value1 != value2);
+let value1 = 10 
+let value2 = 20 
 
-// true , false -> boolean datatype
-console.log(value1 > value2 && value1 < value2 );//false
-console.log(value1 < value2 || value1 < value2 );//true
-let value3 = value1 > value2 && value1 < value2
-console.log(!value3);
+console.log(value1 > value2) // false
+console.log(value2 < value1 )// false
+console.log(value1 <= value2) // true
+console.log(value2 >= value1) // true
+console.log(value1 == value2) // false
+console.log(value1 != value2) // true
 
+// true , false -> boolean data type
+console.log(value1 > value2 && value2 < value1) //false
+console.log(value1 < value2 || value2 < value1) // true
+let value3 = value1 > value2 && value2 < value1
+console.log(!value3)
