dbcc1e69df928c53d89556130ff744b3d2cf0760
maira zahid <mahmoodzahid105@gmail.com>
2026-03-07 00:34:58 -0800

    class no 32

---
+++ classes/JAVASCRIPT/class no 21/javascript.js
@@ -0,0 +1,25 @@
+// use Claude.ai
+
+//increment operator
+let first_no = 2
+first_no++
+console.log(first_no);//3
+first_no = first_no + 1
+console.log(first_no);//4
+
+//Comparison operator
+let value1 = 10
+let value2 = 20
+console.log(value1 > value2);//false
+console.log(value1 < value2); 
+console.log(value1 > value2); 
+console.log(value1 <= value2);
+console.log(value1 == value2);
+console.log(value1 != value2);
+
+// true , false -> boolean datatype
+console.log(value1 > value2 && value1 < value2 );//false
+console.log(value1 < value2 || value1 < value2 );//true
+let value3 = value1 > value2 && value1 < value2
+console.log(!value3);
+
